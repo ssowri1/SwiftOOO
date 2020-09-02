d5b0f3d0f967dbbe3a0fb25c7e0c6fb78b0e4981
ssowri1 <ssowri1@gmail.com>
2020-09-03 00:07:56 +0530

    Added SwiftyLib.podspec

---
+++ SwiftOOO/SwoiftOOO.swift
@@ -0,0 +1,23 @@
+//
+//  SwoiftOOO.swift
+//  SwiftOOO
+//
+//  Created by Sowrirajan S on 02/09/20.
+//  Copyright © 2020 com.ssowri1. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftOOO {
+
+    let name = "SwiftOOO"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
