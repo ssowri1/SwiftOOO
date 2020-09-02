983da9e1b672da4d665a14548eb1df1c2a4d4be9
ssowri1 <ssowri1@gmail.com>
2020-09-02 23:32:34 +0530

    Initial project setup

---
+++ SwiftOOO/SwiftOOO.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOOO.h
+//  SwiftOOO
+//
+//  Created by Sowrirajan S on 02/09/20.
+//  Copyright © 2020 com.ssowri1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftOOO.
+FOUNDATION_EXPORT double SwiftOOOVersionNumber;
+
+//! Project version string for SwiftOOO.
+FOUNDATION_EXPORT const unsigned char SwiftOOOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOOO/PublicHeader.h>
+
+
